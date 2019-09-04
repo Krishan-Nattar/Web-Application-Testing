1faf24d6082b5d7550e01b99985a85f6c0518d43
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-03 19:33:20 -0700

    base components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
 function App() {
+
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+
+  const handleStrike = () => {
+    console.log('strike')
+  }
+
+  const handleBall = () => {
+    console.log('ball')
+  }
+
+  const handleFoul = () => {
+    console.log('foul')
+  }
+
+  const handleHit = () => {
+    console.log('hit')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Web Application Testing
+      <Display balls={balls} strikes={strikes} />
+      <Dashboard handleStrike={handleStrike} handleBall={handleBall} handleFoul={handleFoul} handleHit={handleHit} />
+
     </div>
   );
 }

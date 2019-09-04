794561708c394789905accbc83051d2e53120c6b
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-03 21:12:32 -0700

    testing methods added for all functions

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Display from './components/Display';
-import Dashboard from './components/Dashboard';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
 
   const handleStrike = () => {
-    if(strikes<2){
-      setStrikes(strikes+1);
-    } else{
+    if (strikes < 2) {
+      setStrikes(strikes + 1);
+    } else {
       setStrikes(0);
       setBalls(0);
     }
-    
-  }
+  };
 
   const handleBall = () => {
-    if(balls===3){
+    if (balls === 3) {
       setBalls(0);
       setStrikes(0);
-    } else{
-      setBalls(balls+1);
+    } else {
+      setBalls(balls + 1);
     }
-    
-  }
+  };
 
   const handleFoul = () => {
-    if(strikes<2){
-      setStrikes(strikes+1);
+    if (strikes < 2) {
+      setStrikes(strikes + 1);
     }
-  }
+  };
 
   const handleHit = () => {
-    console.log('hit')
-  }
+    setStrikes(0);
+    setBalls(0);
+  };
 
   return (
     <div className="App">
       At Bat:
       <Display balls={balls} strikes={strikes} />
-      <Dashboard handleStrike={handleStrike} handleBall={handleBall} handleFoul={handleFoul} handleHit={handleHit} />
-
+      <Dashboard
+        handleStrike={handleStrike}
+        handleBall={handleBall}
+        handleFoul={handleFoul}
+        handleHit={handleHit}
+      />
     </div>
   );
 }

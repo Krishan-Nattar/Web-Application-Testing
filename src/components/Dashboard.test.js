f664d20843bc9cc383c88f2d8bc074026f0cf62e
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-03 20:26:12 -0700

    adding tests for dashboard

--- src/components/Dashboard.test.js
+++ src/components/Dashboard.test.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import Dashboard from "./Dashboard";
+import "@testing-library/jest-dom/extend-expect";
+import * as rtl from "@testing-library/react";
 
-import App from './App';
-import '@testing-library/jest-dom/extend-expect';
-import * as rtl from '@testing-library/react';
+// afterEach(rtl.cleanup);
 
-afterEach(rtl.cleanup);
+describe("<Dashboard />", () => {
+
+
+  it("Renders without crashing", () => {
+    rtl.render(<Dashboard />);
+  });
+
+  it("Handles click on strike button", () => {
+    const wrapper = rtl.render(<Dashboard handleStrike={()=>console.log('strike button clicked')} />);
+    const button = wrapper.getByText(/strike/i);
+    expect(button).toBeInTheDocument();
+    rtl.fireEvent.click(button);
+  });
+
+
+});
